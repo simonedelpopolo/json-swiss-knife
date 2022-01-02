e0691bfbf19bb134923f876130d921d7ed75c405
simonedelpopolo <simonedelpopolo@outlook.com>
2022-01-02 19:34:27 +0000

    v1.5.0 ↓
    changed javascript design pattern
    added statement (0,1) to assertion0

--- assertions/assertions.js
+++ assertions/assertions.js
@@ -1,7 +1,8 @@
 // A todo assertions and statements.
 console.time( 'assertions finished ' )
 import { EventEmitter } from 'events'
-import { ok } from 'assert/strict'
+import { is_json } from '../index.js'
+import { deepStrictEqual, ok, rejects } from 'assert/strict'
 
 const AssertionEvent = new EventEmitter()
 
@@ -15,21 +16,52 @@ const Assertions = {
     
     assertion0 : async (  ) => {
         
-        let response
+        let test_results
         
         console.log( '__________________________________________________________________________' )
         
-        console.log( '\x1b[31m assertion title', 0, '\x1b[0m' )
-        console.log( '    \x1b[31m statement title', 0, '\x1b[0m' )
+        console.log( '\x1b[32mfunction is_json.', '\x1b[31massertion ->', 0, '\x1b[0m' )
+        console.log( '  \x1b[32mlisting statements' )
+        console.log( '    function is_json rejects', '\x1b[31mstatement ->', 0, '\x1b[0m' )
+        console.log( '    function is_json returns false', '\x1b[31mstatement ->', 1, '\x1b[0m' )
         
         Assertions.assertion0.statement = {
             
-            '0' : ( ) => {
-                console.log( '    \x1b[31m executing statement', 0, '\x1b[0m\n' )
+            '0' : async ( ) => {
+                console.log( '    \x1b[31m executing statement -> ', 0, '\x1b[0m\n' )
+    
+                await rejects(
+                    is_json( [] ),
+                    
+                    ( error ) => {
+                        test_results = error
+                        
+                        ok( error === 'only string or Buffer are accepted type for `string` argument. Given type: object' )
+        
+                        return true
+                    }
+                )
                 
                 Assertions.assertion0.statement[ '0' ].message = 'test concluded'
                 
-                return response
+                return test_results
+            },
+    
+            '1' : async ( ) => {
+                console.log( '    \x1b[31m executing statement -> ', 1, '\x1b[0m\n' )
+        
+                const jsonString = '{""'
+        
+                try{
+                    test_results = await is_json( jsonString )
+                    deepStrictEqual( test_results, false, 'something wrong should be false' )
+                }catch ( error ) {
+                    test_results = error.message
+                }
+        
+                Assertions.assertion0.statement[ '1' ].message = 'test concluded'
+        
+                return test_results
             },
             
         }
@@ -38,6 +70,11 @@ const Assertions = {
         const response0 = await Assertions.assertion0.statement[ '0' ]()
         console.log( Assertions.assertion0.statement[ '0' ].message )
         console.log( 'returned response -> ',  response0 )
+    
+        console.log( '---------------------------------------------------------------------------' )
+        const response1 = await Assertions.assertion0.statement[ '1' ]()
+        console.log( Assertions.assertion0.statement[ '1' ].message )
+        console.log( 'returned response -> ',  response1 )
         
     },
 }
--- index.js
+++ index.js
@@ -1,192 +1,11 @@
-import * as Module from 'module'
-import { array_, object_, string_ } from 'oftypes'
-
-const json_swiss_knife = Object.create( Module )
-
-const type__ = {
-    
-    /**
-     * It checks if the given argument has the right requested type.
-     *
-     * @param { any | Buffer} variable - The given variable to the string argument.
-     * @returns {Promise|PromiseFulfilledResult<string>|PromiseRejectedResult<string>}
-     */
-    string_buffer: async ( variable ) => {
-        
-        let is_string
-        
-        if( Buffer.isBuffer( variable ) === true )
-            is_string = variable.toString( 'utf-8' )
-        
-        else if ( await string_( variable ) === true )
-            is_string = variable
-        
-        else
-            is_string = false
-        
-        return new Promise( ( resolve, reject ) => {
-            
-            if( is_string === false )
-                reject( false )
-            
-            resolve( is_string )
-            
-        } )
-        
-    },
-    
-    /**
-     * It checks that the argument passed is an array.
-     *
-     * @param {any} variable - The given variable to the array argument.
-     * @returns {Promise<void>}
-     */
-    array: async( variable ) => {
-        
-        let is_array
-        if ( await array_( variable ) === true )
-            is_array = variable
-        else
-            is_array = false
-        
-        return new Promise( ( ( resolve, reject ) => {
-            
-            if( is_array === false )
-                reject( false )
-            
-            resolve( is_array )
-        } ) )
-    }
-}
-
-const parseSymbol = Symbol( 'The function parse(string|buffer):object' )
-Object.defineProperty( json_swiss_knife, parseSymbol, {
-    enumerable:true,
-    configurable:false,
-    writable: false,
-    
-    /**
-     * Check if the given string is valid json syntax before to parse it and return an object.
-     *
-     * @param {Buffer|string} string - The given string.
-     * @returns {Promise | PromiseFulfilledResult<object> | PromiseRejectedResult<string>}
-     */
-    value: async function parse( string ){
-    
-        const type_check = await type__.string_buffer( string ).catch( error => error )
-        
-        return new Promise( ( resolve, reject ) => {
-            
-            if ( type_check === false )
-                reject( `only string or Buffer are accepted type for \`string\` argument. Given type: ${ typeof string }` )
-        
-            try {
-                resolve( JSON.parse( type_check ) )
-            }
-            catch ( error ) {
-                reject( error.message )
-            }
-        } )
-    }
-} )
-
-const property_valueSymbol = Symbol( 'The function property_value(array):string|object' )
-Object.defineProperty( json_swiss_knife, property_valueSymbol, {
-    enumerable: true,
-    configurable: false,
-    writable: false,
-    
-    /**
-     * Given an array it will give back an JSON string object
-     * where, the first entry of the array will be the property name of the object
-     * and the second entry of the array will be the value of the property.
-     * !The given array length must be multiple of two.
-     *
-     * @param {string[]|number[]} array - The give array.
-     * @param {boolean=false} obj - If set to true it will give back an object instead of a json string.
-     * @returns {Promise | PromiseFulfilledResult<|string|object> | PromiseRejectedResult<string>}
-     */
-    value: async function property_value( array, obj= false ) {
-        
-        let is_array
-        is_array = await type__.array( array ).catch( error => error )
-    
-        return new Promise( ( resolve, reject ) => {
-            
-            let json
-            let even = 0
-            let property_value = []
-            
-            if( is_array === false )
-                reject( `this method accept only Array. Given type: ${ typeof array }` )
-            
-            if ( is_array.length % 2 !== 0 )
-                reject( 'this method accept only Array that have at least two entries or multiple of 2' )
-            
-            for ( const key in is_array ) {
-        
-                if ( even + parseInt( key ) % 2 === 0 )
-                    property_value.push( Array.of( is_array[ parseInt( key ) ], is_array[ parseInt( key ) + 1 ] ) )
-        
-            }
-    
-            json = Object.fromEntries( property_value )
-    
-            resolve( obj === false ? JSON.stringify( json ) : json )
-            
-        } )
-    },
-} )
-
-const isJsonSymbol = Symbol( 'The function is_json(Buffer|string):boolean' )
-
-Object.defineProperty( json_swiss_knife, isJsonSymbol, {
-    enumerable: true,
-    configurable: false,
-    writable: false,
-
-    /**
-     * Given a string or Buffer it will return a boolean, true if it is a JSON string false otherwise.
-     *
-     * @param {string | Buffer} string - .
-     * @returns {Promise | PromiseFulfilledResult<boolean> | PromiseRejectedResult<string>}
-     */
-    value: async function is_json( string ) {
-    
-        let is_string
-    
-        if( Buffer.isBuffer( string ) === true )
-            is_string = string.toString( 'utf-8' )
-    
-        else if ( await string_( string ) === true )
-            is_string = string
-    
-        else
-            is_string = false
-        
-        let parsed
-        if( typeof is_string !== 'boolean' ){
-            
-            parsed = await parse( is_string )
-            if( await object_( parsed ) === true )
-                parsed = true
-            
-        }
-        
-        return new Promise( ( resolve, reject ) => {
-            
-            if( is_string === false )
-                reject( `only string or Buffer are accepted type for \`string\` argument. Given type: ${ typeof string }` )
-            
-            if( parsed !== true )
-                resolve( false )
-            
-            resolve( true )
-        } )
-    },
-} )
-
-Object.freeze( json_swiss_knife )
+import {
+    is_json as i_j,
+    is_jsonSymbol as i_js,
+    parse as p,
+    property_value as p_v,
+    property_valueSymbol as p_vs,
+    parseSymbol as ps
+} from './lib/json-swiss-knife/exporter.js'
 
 /**
  * Check if the given string is valid json syntax before to parse it and return an object.
@@ -196,7 +15,7 @@ Object.freeze( json_swiss_knife )
  */
 export async function parse( string ){
     
-    return json_swiss_knife[ parseSymbol ]( string ).then( obj => obj )
+    return p[ ps ]( string ).then( obj => obj )
         .catch( error => error )
 }
 
@@ -204,7 +23,7 @@ export async function parse( string ){
  * Given an array it will give back an JSON string object
  * where, the first entry of the array will be the property name of the object
  * and the second entry of the array will be the value of the property.
- * !The given array length must be multiple of two.
+ * The given array length must be multiple of two.
  *
  * @param {string[]|number[]} array - The give array.
  * @param {boolean=false} obj - If set to true it will give back an object instead of a json string.
@@ -212,7 +31,7 @@ export async function parse( string ){
  */
 export async function property_value( array, obj= false ){
     
-    return json_swiss_knife[ property_valueSymbol ]( array, obj ).then( obj => obj )
+    return p_v[ p_vs ]( array, obj ).then( obj => obj )
         .catch( error => error )
 }
 
@@ -220,10 +39,8 @@ export async function property_value( array, obj= false ){
  * Given a string or Buffer it will return a boolean, true if it is a JSON string false otherwise.
  *
  * @param {string | Buffer} string - .
- * @returns {Promise | PromiseFulfilledResult<boolean> | PromiseRejectedResult<string>}
  */
-export async function is_json( string ){
+export function is_json( string ){
     
-    return json_swiss_knife[ isJsonSymbol ]( string ).then( boolean => boolean )
-        .catch( error => error )
+    return i_j[ i_js ]( string )
 }
